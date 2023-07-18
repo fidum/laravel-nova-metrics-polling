202f636be5fcd5e6eee2b36d576e9df0d37c5327
dmason30 <danmason@spendology.com>
2023-07-18 18:10:35 +0100

    wip

--- src/Concerns/SupportsPolling.php
+++ src/Concerns/SupportsPolling.php
@@ -4,13 +4,17 @@ namespace Fidum\LaravelNovaMetricsPolling\Concerns;
 
 trait SupportsPolling
 {
-    public function refreshIntervalMilliseconds(int $milliseconds): self
+    public function refreshIntervalMilliseconds(int|callable $milliseconds): self
     {
+        $milliseconds = (int) value($milliseconds);
+
         return $this->withMeta(['pollingInterval' => max(1000, $milliseconds)]);
     }
 
-    public function refreshIntervalSeconds(int $seconds): self
+    public function refreshIntervalSeconds(int|callable $seconds): self
     {
+        $seconds = (int) value($seconds);
+
         return $this->refreshIntervalMilliseconds($seconds * 1000);
     }
 }
--- tests/Unit/Concerns/SupportsPollingTest.php
+++ tests/Unit/Concerns/SupportsPollingTest.php
@@ -4,6 +4,8 @@ use Fidum\LaravelNovaMetricsPolling\Concerns\SupportsPolling;
 
 uses(SupportsPolling::class);
 
+afterEach(fn () => $this->meta = []);
+
 test('refreshIntervalMilliseconds with 0 milliseconds sets the polling interval to the minimum 1000 milliseconds')
     ->expect(fn () => $this->refreshIntervalMilliseconds(0))
     ->meta
@@ -14,8 +16,23 @@ test('refreshIntervalMilliseconds with 100 milliseconds sets the polling interva
     ->meta
     ->toBe(['pollingInterval' => 1000]);
 
-test('refreshIntervalMilliseconds with 10000 milliseconds sets the polling interval to 10000 milliseconds')
-    ->expect(fn () => $this->refreshIntervalMilliseconds(100))
+test('refreshIntervalMilliseconds with a closure returning 100 milliseconds sets the polling interval to the minimum 1000 milliseconds')
+    ->expect(fn () => $this->refreshIntervalMilliseconds(fn () => 100))
+    ->meta
+    ->toBe(['pollingInterval' => 1000]);
+
+test('refreshIntervalMilliseconds with 1000 milliseconds sets the polling interval to 10000 milliseconds')
+    ->expect(fn () => $this->refreshIntervalMilliseconds(10000))
+    ->meta
+    ->toBe(['pollingInterval' => 10000]);
+
+test('refreshIntervalMilliseconds with a closure returning 1000 milliseconds sets the polling interval yo 10000 milliseconds')
+    ->expect(fn () => $this->refreshIntervalMilliseconds(fn () => 1000))
+    ->meta
+    ->toBe(['pollingInterval' => 1000]);
+
+test('refreshIntervalMilliseconds with a closure returning a non-numeric value sets the polling interval to the minimum 1000 milliseconds')
+    ->expect(fn () => $this->refreshIntervalMilliseconds(fn () => 'NaN'))
     ->meta
     ->toBe(['pollingInterval' => 1000]);
 
@@ -29,7 +46,22 @@ test('refreshIntervalSeconds with 1 seconds sets the polling interval to 1000 mi
     ->meta
     ->toBe(['pollingInterval' => 1000]);
 
+test('refreshIntervalSeconds with closure returning 1 seconds sets the polling interval to 1000 milliseconds')
+    ->expect(fn () => $this->refreshIntervalSeconds(fn () => 1))
+    ->meta
+    ->toBe(['pollingInterval' => 1000]);
+
 test('refreshIntervalSeconds with 10 seconds sets the polling interval to 10000 milliseconds')
     ->expect(fn () => $this->refreshIntervalSeconds(10))
     ->meta
     ->toBe(['pollingInterval' => 10000]);
+
+test('refreshIntervalSeconds with with closure returning 10 seconds sets the polling interval to 10000 milliseconds')
+    ->expect(fn () => $this->refreshIntervalSeconds(fn () => 10))
+    ->meta
+    ->toBe(['pollingInterval' => 10000]);
+
+test('refreshIntervalSeconds with a closure returning a non-numeric value sets the polling interval to the minimum 1000 milliseconds')
+    ->expect(fn () => $this->refreshIntervalSeconds(fn () => 'NaN'))
+    ->meta
+    ->toBe(['pollingInterval' => 1000]);
